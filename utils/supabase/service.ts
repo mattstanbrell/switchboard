e4a39eace74bc79e80a4abc7bd5a4559779dec8f
Gauntlet <mattstanbrell@gmail.com>
2025-01-27 22:44:36 +0000

    update env var name

--- utils/supabase/service.ts
+++ utils/supabase/service.ts
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 // Create a Supabase client with the service role key
 export const supabaseService = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
   {
     auth: {
       autoRefreshToken: false,
